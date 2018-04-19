3c4fc4e7a103049b86cfb787e41139cb2983cb2c
alpag <pawlaq123@gmail.com>
2018-04-19 15:17:13 +0200

    init admin panel

--- guide/guidebase/admin.py
+++ guide/guidebase/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import TypeOfPlace
+from .models import Place
+
+admin.site.register(TypeOfPlace)
+admin.site.register(Place)
 # Register your models here.
