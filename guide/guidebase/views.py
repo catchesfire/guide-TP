179caa7bba0d59ad49189e41cd1d60e2a8664f4a
alpag <pawlaq123@gmail.com>
2018-04-23 11:35:38 +0200

    init index html

--- guide/guidebase/views.py
+++ guide/guidebase/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. I fuck dog when he shits")
+    template = loader.get_template('guidebase/index.html')
+    return HttpResponse(template.render())
 # Create your views here.
+
