f06fcf115c2f8207240adb9eaf073ff3f0f60105
alpag <pawlaq123@gmail.com>
2018-05-05 16:01:01 +0200

    connect to db (error)

--- guide/guidebase/views.py
+++ guide/guidebase/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import models
+from .models import Place
 from django.template import loader
 
 
+
 def index(request):
-    template = loader.get_template('guidebase/index.html')
-    return HttpResponse(template.render())
+    all_places = Place.objects.all()
+    html = ''
+    for Place in all_places:
+        url = '/guidebase/' + str(Place.id)
+        html += '<a href="' + url + '">' + Place.name + '</a><br>'
+
+    return HttpResponse(html)
 # Create your views here.
 
 
 def detail(request, place_id):
-    return HttpResponse("<h2>Szczegoly dla " + str(place_id) + "</h2>")
+    return HttpResponse("<h2>Szczegóły dla " + str(place_id) + "</h2>")
+    # template = loader.get_template('guidebase/index.html')
